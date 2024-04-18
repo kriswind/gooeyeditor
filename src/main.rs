5bdf50e8c821ee50bdcdcbf5e800ec8e57e2c0d5
kriswind <126849955+kriswind@users.noreply.github.com>
2024-04-18 18:50:31 +0200

    added final sandbox elements, cursor indicator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use iced::widget::{container, text_editor};
-use iced::{Element, Sandbox, Settings};
+use iced::widget::{column, container, horizontal_space, row, text, text_editor};
+use iced::{Element, Length, Sandbox, Settings, Theme};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
@@ -19,7 +19,7 @@ impl Sandbox for Editor {
 
     fn new() -> Self {
         Self {
-            content: text_editor::Content::new(),
+            content: text_editor::Content::with(include_str!("main.rs")),
         }
     }
 
@@ -38,6 +38,20 @@ impl Sandbox for Editor {
     fn view(&self) -> Element<'_, Message> {
         let input = text_editor(&self.content).on_edit(Message::Edit);
 
-        container(input).padding(10).into()
+        let position = {
+            let (line, column) = self.content.cursor_position();
+
+            text(format!("{}:{}", line + 1, column + 1))
+        };
+
+        let status_bar = row![horizontal_space(Length::Fill), position];
+
+        container(column![input, status_bar].spacing(10))
+            .padding(10)
+            .into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::Dark
     }
 }
