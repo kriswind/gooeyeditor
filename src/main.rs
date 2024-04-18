e57c3c22cf45c3986fb926df3e8cfeaf5af28ed1
kriswind <126849955+kriswind@users.noreply.github.com>
2024-04-18 16:52:53 +0200

    created ice window

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use iced::widget::{container, text_editor};
+use iced::{Element, Sandbox, Settings};
+
+fn main() -> iced::Result {
+    Editor::run(Settings::default())
+}
+
+struct Editor {
+    content: text_editor::Content,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+}
+
+impl Sandbox for Editor {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self {
+            content: text_editor::Content::new(),
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("A cool editor!")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Edit(action) => {
+                self.content.edit(action);
+            }
+        }
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        let input = text_editor(&self.content).on_edit(Message::Edit);
+
+        container(input).padding(10).into()
+    }
+}
