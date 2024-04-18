8d9d11f1bd8587c53fef15bc26fb0437e140e38e
kriswind <126849955+kriswind@users.noreply.github.com>
2024-04-18 19:18:09 +0200

    added async functionality with tokio

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::io;
+use std::path::Path;
+use std::sync::Arc;
+
 use iced::widget::{column, container, horizontal_space, row, text, text_editor};
-use iced::{Element, Length, Sandbox, Settings, Theme};
+use iced::{executor, Application, Command, Element, Length, Settings, Theme};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
@@ -12,27 +16,44 @@ struct Editor {
 #[derive(Debug, Clone)]
 enum Message {
     Edit(text_editor::Action),
+    FileOpened(Result<Arc<String>, io::ErrorKind>),
 }
 
-impl Sandbox for Editor {
+impl Application for Editor {
     type Message = Message;
+    type Theme = Theme;
+    type Executor = executor::Default;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self {
-            content: text_editor::Content::with(include_str!("main.rs")),
-        }
+    fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
+        (
+            Self {
+                content: text_editor::Content::new(),
+            },
+            Command::perform(
+                load_file(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR"),)),
+                Message::FileOpened,
+            ),
+        )
     }
 
     fn title(&self) -> String {
         String::from("A cool editor!")
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::Edit(action) => {
                 self.content.edit(action);
             }
+            Message::FileOpened(result) => {
+                if let Ok(content) = result {
+                    self.content = text_editor::Content::with(&content);
+                }
+            }
         }
+
+        Command::none()
     }
 
     fn view(&self) -> Element<'_, Message> {
@@ -54,4 +75,11 @@ impl Sandbox for Editor {
     fn theme(&self) -> Theme {
         Theme::Dark
     }
+}
+
+async fn load_file(path: impl AsRef<Path>) -> Result<Arc<String>, io::ErrorKind> {
+    tokio::fs::read_to_string(path)
+        .await
+        .map(Arc::new)
+        .map_err(|error| error.kind())
 }
